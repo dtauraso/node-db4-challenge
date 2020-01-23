741b4dc57e4bcebc0099b3608d964a481ea3306a
David Tauraso <david318@me.com>
2020-01-23 13:07:25 -0800

    Started project

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+
+// const RecipiesRouter = require('./recipies/scheme-router.js');
+
+const server = express();
+
+server.use(express.json());
+server.use(logger)
+// server.use('/api/recipies', RecipiesRouter);
+
+
+function logger(req, res, next) {
+    const { method, originalUrl } = req
+    console.log(`${method} to ${originalUrl}`)
+    next()
+}
+module.exports = server;
